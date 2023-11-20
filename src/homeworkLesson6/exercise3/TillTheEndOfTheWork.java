95df1a734fb5612de4a2a2a233be1fc01c1935e3
SergeiVS <fam.smirnoff@gmail.com>
2023-11-20 18:25:36 +0100

    test

---
+++ src/homeworkLesson6/exercise3/TillTheEndOfTheWork.java
@@ -0,0 +1,13 @@
+package homeworkLesson6.exercise3;
+
+public class TillTheEndOfTheWork {
+    public static void main(String[] args) {
+
+double n = Math.random() * (28800 + 1);
+
+        System.out.println("Till the end of the work is only " + (int)n + " seconds.");
+
+        System.out.println("Till the end of the work is only " + (int)(n / 3600) + " hours.");
+    }
+
+}
