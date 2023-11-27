6144f4d6d709eb63083e916eeb7824d0d20c0bf0
SergeiVS <fam.smirnoff@gmail.com>
2023-11-27 17:54:21 +0100

    test

--- src/lesson6/exercise5BankNew/OperationsCount.java
+++ src/lesson6/exercise5BankNew/OperationsCount.java
@@ -7,11 +7,11 @@ public class OperationsCount {
         AtmUtil atmUtil = new AtmUtil();
         int balance = atmUtil.getBalance();
         int triesCount = 0;
-        System.out.println("Your accound balance is: " + balance);
+        System.out.println("Your account balance is: " + balance);
 
         while (balance>0){
             int draw = atmUtil.getDraw(balance);
-                balance = balance - draw;
+                balance -= draw;
                 triesCount++;
                System.out.println("At the day " + triesCount + " your balance is: " + balance);
             }
