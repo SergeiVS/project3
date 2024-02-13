35a57a53c2e7bf3e9b6654b913c6f9fadfad36a5
SergeiVS <fam.smirnoff@gmail.com>
2024-02-13 10:04:29 +0100

    test

--- src/lesson35/homework/task4/repository/RepositoryAnalyze.java
+++ src/lesson35/homework/task4/repository/RepositoryAnalyze.java
@@ -18,51 +18,65 @@ public class RepositoryAnalyze {
     //   c) V выбрать топ 10 самых посещаемых матчей
     //   d) V выбрать топ 3 матчей с самой большой разницей в счете на стадионе WWK Arena
     //   e)  V посчитать сколько матчей содержат упоминания о стадионе
-    //   f) посчитать сколько матчей сыграла каждая команда в качестве гостя (в формате map)
+    //   f)  V посчитать сколько матчей сыграла каждая команда в качестве гостя (в формате map)
     //   g) V выделить наиболее популярный день проведения матча
 
-    public List<Match> matchesByPeriod(List<Match> matches, Integer endDay){
+    public List<Match> matchesByPeriod(List<Match> matches, Integer endDay) {
         List<Match> result = matches.stream()
-                .filter(match ->  match.getDate().get(Calendar.DAY_OF_MONTH) <= endDay)
+                .filter(match -> match.getDate().get(Calendar.DAY_OF_MONTH) <= endDay)
                 .toList();
         return result;
     }
 
 
-
-    public Integer matchesCount(List<Match> matches){
+    public Integer matchesCount(List<Match> matches) {
         return matches.size();
     }
-    public List<Match> topMatchesByWatchers(List<Match> matches, long rangeLength){
+
+    public List<Match> topMatchesByWatchers(List<Match> matches, long rangeLength) {
         return matches.stream()
                 .sorted((m1, m2) -> m1.getWatchers().compareTo(m2.getWatchers()))
                 .limit(rangeLength)
                 .toList();
     }
-    public Integer matchesWithStadiumAvailable(List<Match> matches){
+
+    public Integer matchesWithStadiumAvailable(List<Match> matches) {
         List<Match> matchList = matches.stream()
                 .filter(match -> !match.getStadium().equals("N/A"))
                 .toList();
         return matches.size();
     }
-    public List<Match> matchesByStadium(List<Match> matches, String stadium){
-       return matches.stream()
+
+    public List<Match> matchesByStadium(List<Match> matches, String stadium) {
+        return matches.stream()
                 .filter(match -> match.getStadium().equals(stadium))
                 .toList();
     }
 
-    public Map<Integer, Long> mostPopularDay(List<Match> matches, Integer topLimit){
+    public Map<Integer, Long> mostPopularDay(List<Match> matches, Integer topLimit) {
 
-       Map<Integer, Long> map = matches.stream()
+        Map<Integer, Long> map = matches.stream()
                 .collect(Collectors.groupingBy(m -> m.getDate().get(Calendar.DAY_OF_WEEK), Collectors.counting()));
         return map.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(topLimit)
-                .collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue, (e1,e2) -> e1, LinkedHashMap :: new));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
+    public Map<String, Long> guestMatchesCount(List<Match> matches) {
+        return matches.stream()
+                .collect(Collectors.groupingBy(Match::getTeam2, Collectors.counting()));
+    }
 
+    public Map<String, Long> homeMatchesCount(List<Match> matches) {
+        return matches.stream()
+                .collect(Collectors.groupingBy(Match::getTeam1, Collectors.counting()));
+    }
 
-
-
+    public List<Match> highestScoreDifference(List<Match> matches, Integer topLimit) {
+        return matches.stream()
+                .sorted((m1, m2) -> Math.abs(m2.getResult1() - m2.getResult2()) - Math.abs(m1.getResult1() - m2.getResult2()))
+                .limit(topLimit)
+                .toList();
+    }
 }
