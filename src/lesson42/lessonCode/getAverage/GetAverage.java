d963915428ee73dee340bda8fc79319efd51a2f5
SergeiVS <fam.smirnoff@gmail.com>
2024-02-24 17:57:51 +0100

    test

--- src/lesson42/lessonCode/getAverage/GetAverage.java
+++ src/lesson42/lessonCode/getAverage/GetAverage.java
@@ -2,15 +2,18 @@ package lessonCode.getAverage;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 public class GetAverage {
 
     public Double getAverage(List<Integer> integers){
-        int div = integers.size();
-        if (div > 0) {
-            Integer sum = integers.stream()
-                    .reduce(0, (a, b) ->  a + b);
-            return (double) sum / div;
+
+
+            OptionalDouble avg = integers.stream()
+                    .mapToInt(i -> i)
+                    .average();
+            if (avg.isPresent()){
+            return avg.getAsDouble();
         }
        return 0.0;
     }
